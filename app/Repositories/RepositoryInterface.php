9864c9c560944275904cb8b010b2ac9af9896a75
luatvd95 <vu.duy.luat@framgia.com>
2018-10-08 17:22:48 +0700

    update new

---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories;
+
+interface RepositoryInterface {
+    public function all();
+    public function find($id);
+}
---
+++ app/Repositories/SinhvienRepository.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Sinhvien;
+use App\Repositories\RepositoryInterface;
+
+class SinhvienRepository implements RepositoryInterface
+{
+    public function all()
+    {
+        $data = Sinhvien::all();
+        return $data;
+    }
+    public function find($id)
+    {
+        $data = Sinhvien::find($id);
+        return $data;
+    }
+
+}
